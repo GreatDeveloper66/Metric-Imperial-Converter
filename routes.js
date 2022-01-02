9e0f2dfc432e65ec92c2f43870f1b4f118ee6928
Adam Shaffer <ashaffe@gmail.com>
2022-01-02 05:59:33 -0600

    add route for /auth/github

--- routes.js
+++ routes.js
@@ -40,6 +40,9 @@ module.exports = function (app, myDataBase) {
             res.redirect('/profile');
         }
     );
+
+    app.route('/auth/github').get(passport.authenticate('github'));
+
     app.use((req, res, next) => {
         res.status(404).type('text').send('Not Found');
     });
