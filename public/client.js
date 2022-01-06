ce4a63faff611805cdd7bdd685dc799b1ee0843a
Adam Shaffer <ashaffe@gmail.com>
2022-01-05 18:09:40 -0600

    Add socket on user count in client.js

--- public/client.js
+++ public/client.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
   // Form submittion with new message in field with id 'm'
   let socket = io()
+
+  socket.on('user count', function(data) {
+    console.log(data)
+  })
   $('form').submit(function () {
     var messageToSend = $('#m').val();
 
